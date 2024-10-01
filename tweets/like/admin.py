8f04830f1f599fed04cdcaae256a5d92e4957011
jyooDev <gina07099@gmail.com>
2024-10-01 01:04:13 -0500

    feat: added search filters for tweet and like apps

--- tweets/like/admin.py
+++ tweets/like/admin.py
@@ -4,3 +4,9 @@ from .models import Like
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
     list_display = ("user", "tweet", "created_at", "updated_at")
+    list_filter = (
+        "created_at",
+    )
+    search_fields=(
+        "^user__username",
+    )
--- tweets/tweet/admin.py
+++ tweets/tweet/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
 from .models import Tweet
+from django.db.models import Q
+
 # Register your models here.
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
+    class ElonMuskFilter(admin.SimpleListFilter):
+        title = "Filter by Elon Musk"
+
+        parameter_name = "Elon Musk"
+        def lookups(self, request, model_admin):
+            return [
+                ("contains", "contains Elon Musk"),
+                ("not_contains", "not contains Elon Musk"),
+            ]
+        def queryset(self, request, tweet):
+            ELON_MUSK = "Elon Musk"
+            if(self.value() == "contains"):
+                return tweet.filter(payload__icontains=ELON_MUSK)
+            elif(self.value() == "not_contains"):
+                return tweet.filter(~Q(payload__icontains=ELON_MUSK))
+
     list_display = ("user", "payload", "likes_ct", "created_at", "updated_at")
+    list_filter =(
+        "created_at",
+        ElonMuskFilter,
+    )
+    search_fields=(
+        "payload",
+        "^user__username"
+    )
